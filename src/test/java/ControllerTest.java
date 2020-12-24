f1858a7a1f62b8202c3915b256621aa59c562d89
Miodrag Jovanovic <miodrag.jovanovic@vicert.com>
2020-12-24 14:47:40 +0100

    Break test

--- src/test/java/ControllerTest.java
+++ src/test/java/ControllerTest.java
@@ -24,7 +24,7 @@ public class ControllerTest {
         RestAssured
                 .get("/test")
                 .then()
-                .statusCode(200);
+                .statusCode(201);
     }
 
 }
